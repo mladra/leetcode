def28f99aa65ae8bc37905cbc20c9448255dd7d6
Michał Ladra <michal.ladra.95@gmail.com>
2024-05-19 09:09:05 +0200

    Leet Code tasks

--- src/test/java/com/code/leet/easy/FindTheIndexOfTheFirstOccurrenceInAStringLeetCodeTest.java
+++ src/test/java/com/code/leet/easy/FindTheIndexOfTheFirstOccurrenceInAStringLeetCodeTest.java
@@ -9,7 +9,7 @@ import java.util.stream.Stream;
 
 public class FindTheIndexOfTheFirstOccurrenceInAStringLeetCodeTest {
 
-    public int strStr(String haystack, String needle) {
+    public int strStr2(String haystack, String needle) {
         if (haystack == null || haystack.isEmpty()) return -1;
         if (needle == null || needle.isEmpty()) return -1;
 
@@ -22,17 +22,44 @@ public class FindTheIndexOfTheFirstOccurrenceInAStringLeetCodeTest {
         return -1;
     }
 
-    @ParameterizedTest
+    public int strStr(String haystack, String needle) {
+        if (haystack == null || haystack.isEmpty()) return -1;
+        if (needle == null || needle.isEmpty()) return -1;
+
+        for (int i = 0; i < haystack.length(); i++) {
+            if (i + needle.length() > haystack.length()) return -1;
+            boolean isCorrect = true;
+            for (int j = 0; j < needle.length(); j++) {
+                if (haystack.charAt(i + j) != needle.charAt(j)) {
+                    isCorrect = false;
+                    break;
+                }
+            }
+            if (isCorrect) return i;
+        }
+
+        return -1;
+    }
+
+    @ParameterizedTest(name = "solution1: haystack={0}, needle={1}, idx={2}")
     @MethodSource("testInput")
     void test(String haystack, String needle, int expectedIndex) {
         int index = strStr(haystack, needle);
         Assertions.assertEquals(expectedIndex, index);
     }
 
+    @ParameterizedTest(name = "solution2: haystack={0}, needle={1}, idx={2}")
+    @MethodSource("testInput")
+    void test2(String haystack, String needle, int expectedIndex) {
+        int index = strStr2(haystack, needle);
+        Assertions.assertEquals(expectedIndex, index);
+    }
+
     private static Stream<Arguments> testInput() {
         return Stream.of(
                 Arguments.of("sadbutsad", "sad", 0),
-                Arguments.of("leetcode", "leeto", -1)
+                Arguments.of("leetcode", "leeto", -1),
+                Arguments.of("a", "a", 0)
         );
     }
 
